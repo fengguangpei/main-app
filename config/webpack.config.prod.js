4c28077d2361bb890b975fdf127787095eff84c5
fenggp <18319407429@163.com>
2024-12-18 20:32:06 +0800

    sentry配置

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -2,9 +2,11 @@ const { default: merge } = require('webpack-merge')
 const base = require('./webpack.config.base')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { DefinePlugin } = require('webpack')
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 
 module.exports = merge(base({ mode: 'production' }), {
   mode: 'production',
+  devtool: 'source-map',
   output: {
     publicPath: 'https://fenggp.obs.cn-south-1.myhuaweicloud.com/main-app/',
     clean: true
@@ -31,6 +33,12 @@ module.exports = merge(base({ mode: 'production' }), {
       __VUE_OPTIONS_API__: false,
       PRODUCTION: true
     }),
+    sentryWebpackPlugin({
+      org: 'fengguangpei',
+      project: 'oa-main-app',
+      authToken: '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',
+      telemetry: false
+    })
   ],
   optimization: {
     runtimeChunk: 'single',
